2333371a824c4fe4ef761d52e4d1bc0cc6635f9c
Joao Gabriel Maciel <joaogabriel36@hotmail.com>
2020-01-03 10:25:33 -0300

    Primeira classe: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,11 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		
+		Position pos = new Position(20, 30);
+		System.out.println(pos);
 	}
 }
