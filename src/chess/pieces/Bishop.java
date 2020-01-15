c23d1d4daa7a5b2231cd349bdcf3c544789eb870
Joao Gabriel Maciel <joaogabriel36@hotmail.com>
2020-01-15 11:07:58 -0300

    Bispo

--- src/chess/pieces/Bishop.java
+++ src/chess/pieces/Bishop.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -12,7 +13,59 @@ public class Bishop extends ChessPiece {
 
 	@Override
 	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getTabuleiro().getLinhas()][getTabuleiro().getColunhas()];
+		
+		Position p = new Position(0, 0);
+		
+		/* Noroeste */
+		
+		p.setValues(posicao.getLinha() - 1, posicao.getColuna() - 1);
+		while(getTabuleiro().positionExists(p) && !getTabuleiro().thereIsAPiece(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setValues(p.getLinha() - 1, p.getColuna() -1);
+		}
+		if (getTabuleiro().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		/* Nordeste */
+		
+		p.setValues(posicao.getLinha() - 1, posicao.getColuna() + 1);
+		while(getTabuleiro().positionExists(p) && !getTabuleiro().thereIsAPiece(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setValues(p.getLinha() - 1, p.getColuna() + 1);
+		}
+		if (getTabuleiro().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		/* Sudeste */
+		
+		p.setValues(posicao.getLinha() + 1, posicao.getColuna() + 1);
+		while(getTabuleiro().positionExists(p) && !getTabuleiro().thereIsAPiece(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setValues(p.getLinha() + 1, p.getColuna() + 1);
+		}
+		if (getTabuleiro().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		/* Sudoeste */
+		
+		p.setValues(posicao.getLinha() + 1, posicao.getColuna() - 1);
+		while(getTabuleiro().positionExists(p) && !getTabuleiro().thereIsAPiece(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+			p.setValues(p.getLinha() + 1, p.getColuna() - 1);
+		}
+		if (getTabuleiro().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
 		return null;
+	}
+	
+	@Override
+	public String toString() {
+		return "B";
 	}
 	
 }
